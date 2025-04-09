633c7c7ebdb7ea4dfb4e802f3f88a4a0ddde2361
box03 <box03-853@yandex.ru>
2025-04-09 12:29:35 +0400

    тест

---
+++ src/main/java/com/example/TrialTraining/TrialTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.example.TrialTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialTrainingApplication.class, args);
+		System.out.println();
+	}
+
+}
---
+++ src/test/java/com/example/TrialTraining/TrialTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrialTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
